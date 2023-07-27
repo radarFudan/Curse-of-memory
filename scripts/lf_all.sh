5c6015eb35e10d2007fa18257eaa19b46c1ccebe
Altair <25528016+radarFudan@users.noreply.github.com>
2023-07-27 20:22:58 +0800

    clean up scripts, plan to make task name into wandb name

--- scripts/lf_all.sh
+++ scripts/lf_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 activation_list=("linear" "hardtanh" "tanh") # recurrent activation
-model_list=("diagonalrnn" "rnn" "softplusrnn") # parameterization method
+model_list=("diagonalrnn" "rnn" "softplusrnn" "lru" "ssm" "softplusssm" "s4") # parameterization method
 rho_name_list=("exp" "pol")
 
 for activation in "${activation_list[@]}"
@@ -12,39 +12,46 @@ do
         do
             experiment="LF/lf-${model}"
             task_name="LF_${activation}_${model}_${rho_name}"
-            log_dir_path="logs/${task_name}/runs/"
+            log_dir_path="logs/${task_name}/runs"
 
             rec1_size_list=("8" "16" "32" "64")
             metric_value=("100")
-            ckpt_path=()
             ckpt_path_file="${log_dir_path}/ckpt_path.txt"
             trained=False
 
+            gpu_index="0"
+
             if [ "$trained" = False ]
             then
+                echo "clean up ckpt_path_file and metric_value"
+                echo -n > "$ckpt_path_file"
+                echo -n > "${log_dir_path}/metric_value.txt"
+
                 for i in "${!rec1_size_list[@]}"
                 do
-                    python src/train.py experiment="${experiment}" data.rho_name="${rho_name}" model.net.rec1_size="${rec1_size_list[$i]}" model.net.activation="${activation}" task_name="${task_name}" callbacks.early_stopping.stopping_threshold="${metric_value[-1]}" logger=many_loggers
+                    CUDA_VISIBLE_DEVICES=$gpu_index python src/train.py experiment="${experiment}" data.rho_name="${rho_name}" model.net.rec1_size="${rec1_size_list[$i]}" model.net.activation="${activation}" task_name="${task_name}" callbacks.early_stopping.stopping_threshold="${metric_value[-1]}" logger=many_loggers
 
-                    ckpt_path+=("$(cat ckpt_path.txt)")
-                    echo "${ckpt_path[-1]}" >> "$ckpt_path_file"
-
-                    metric_value+=("$(cat metric_value.txt)")
+                    metric_value+=("$(cat "${log_dir_path}/metric_value.txt")")
                     echo "Ensure approximation: "
                     echo "${metric_value[@]}"
                 done
             else
                 echo "The model is already trained."
-                while IFS= read -r line
-                do
-                    ckpt_path+=("$line")
-                done < "$ckpt_path_file"
             fi
 
-
-            for i in "${!rec1_size_list[@]}"
+            # Read checkpoint paths
+            ckpt_path=()
+            while IFS= read -r line
             do
-                python src/perturb.py experiment="${experiment}" data.rho_name="${rho_name}" model.net.rec1_size="${rec1_size_list[$i]}" model.net.activation="${activation}" task_name="${task_name}_PERTURB" ckpt_path="${ckpt_path[$i]}" +model.net.training=False +perturb_range=21 +perturb_repeats=30 +perturbation_interval=0.5
+                ckpt_path+=("$line")
+            done < "$ckpt_path_file"
+
+            echo "checkpoints paths:"
+            echo "${ckpt_path[@]}"
+
+            for i in "${!ckpt_path[@]}"
+            do
+                CUDA_VISIBLE_DEVICES=$gpu_index python src/perturb.py experiment="${experiment}" data.rho_name="${rho_name}" model.net.rec1_size="${rec1_size_list[$i]}" model.net.activation="${activation}" task_name="${task_name}_PERTURB" logger=many_loggers ckpt_path="${ckpt_path[$i]}" +model.net.training=False +perturb_range=21 +perturb_repeats=3 +perturbation_interval=0.5
             done
         done
     done
--- scripts/lf_parallelized.sh
+++ scripts/lf_parallelized.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 activation_list=("linear" "hardtanh" "tanh") # recurrent activation
-model_list=("diagonalrnn" "rnn" "softplusrnn") # parameterization method
+model_list=("diagonalrnn" "rnn" "softplusrnn" "lru" "ssm" "softplusssm" "s4") # parameterization method
 rho_name_list=("exp" "pol")
 
 train_and_perturb() {
     activation=$1
     model=$2
     rho_name=$3
-    experiment="LF/lf-${model}"
 
+    experiment="LF/lf-${model}"
     task_name="LF_${activation}_${model}_${rho_name}"
     log_dir_path="logs/${task_name}/runs"
 
@@ -19,13 +19,16 @@ train_and_perturb() {
     trained=False
 
     gpu_index=$(((PARALLEL_SEQ-1)%4))  # Cycle through GPU indices 0,1,2,3
-    export CUDA_VISIBLE_DEVICES=$gpu_index  # Set CUDA_VISIBLE_DEVICES for this job
 
     if [ "$trained" = False ]
     then
+        echo "clean up ckpt_path_file and metric_value"
+        echo -n > "$ckpt_path_file"
+        echo -n > "${log_dir_path}/metric_value.txt"
+
         for i in "${!rec1_size_list[@]}"
         do
-            python src/train.py experiment="${experiment}" data.rho_name="${rho_name}" model.net.rec1_size="${rec1_size_list[$i]}" model.net.activation="${activation}" task_name="${task_name}" callbacks.early_stopping.stopping_threshold="${metric_value[-1]}" logger=many_loggers
+            CUDA_VISIBLE_DEVICES=$gpu_index python src/train.py experiment="${experiment}" data.rho_name="${rho_name}" model.net.rec1_size="${rec1_size_list[$i]}" model.net.activation="${activation}" task_name="${task_name}" callbacks.early_stopping.stopping_threshold="${metric_value[-1]}" logger=many_loggers
 
             metric_value+=("$(cat "${log_dir_path}/metric_value.txt")")
             echo "Ensure approximation: "
@@ -47,7 +50,7 @@ train_and_perturb() {
 
     for i in "${!ckpt_path[@]}"
     do
-        python src/perturb.py experiment="${experiment}" data.rho_name="${rho_name}" model.net.rec1_size="${rec1_size_list[$i]}" model.net.activation="${activation}" task_name="${task_name}_PERTURB" logger=many_loggers ckpt_path="${ckpt_path[$i]}" +model.net.training=False +perturb_range=21 +perturb_repeats=3 +perturbation_interval=0.5
+        CUDA_VISIBLE_DEVICES=$gpu_index python src/perturb.py experiment="${experiment}" data.rho_name="${rho_name}" model.net.rec1_size="${rec1_size_list[$i]}" model.net.activation="${activation}" task_name="${task_name}_PERTURB" logger=many_loggers ckpt_path="${ckpt_path[$i]}" +model.net.training=False +perturb_range=21 +perturb_repeats=30 +perturbation_interval=0.5
     done
 }
 
